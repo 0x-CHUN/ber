30c6373fcbbc38d8d9e13f684bdad6468e89f9f5
0x-CHUN <chunhui.xie@qq.com>
2023-03-23 15:46:57 +0800

    complete css parse

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use std::fs::File;
 use std::io::Read;
-use crate::dom::ElementData;
 
 pub mod dom;
 pub mod html;
+pub mod css;
 
 fn main() {
     let html_content = read("./tests/test.html".to_string());
     let root_node = html::parse(html_content);
-    println!("{:?}", root_node);
     assert_eq!(root_node.children.len(), 2);
     assert_eq!(root_node.children.get(0).unwrap().children.len(), 1);
     assert_eq!(root_node.children.get(1).unwrap().children.len(), 2);
+
+    let css_content = read("./tests/test.css".to_string());
+    let style = css::parse(css_content);
+    assert_eq!(style.rules.len(), 8);
 }
 
 fn read(filename: String) -> String {
