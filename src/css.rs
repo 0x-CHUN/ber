7d2cdcc87cfe61d73386d3adaf4b44d8c6f58517
0x-CHUN <chunhui.xie@qq.com>
2023-03-24 15:44:24 +0800

    complete painting and pdf

--- src/css.rs
+++ src/css.rs
@@ -39,7 +39,7 @@ pub enum Unit {
     Px,
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Copy)]
 pub struct Color {
     pub r: u8,
     pub g: u8,
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,67 @@
+extern crate getopts;
+extern crate image;
+
 use std::fs::File;
-use std::io::Read;
+use std::io::{BufWriter, Read};
+use image::Rgba;
 
 pub mod dom;
 pub mod html;
 pub mod css;
 pub mod style;
 pub mod layout;
+pub mod painting;
+pub mod pdf;
 
 fn main() {
-    let html_content = read("./tests/test.html".to_string());
-    let root_node = html::parse(html_content);
-    assert_eq!(root_node.children.len(), 2);
-    assert_eq!(root_node.children.get(0).unwrap().children.len(), 1);
-    assert_eq!(root_node.children.get(1).unwrap().children.len(), 2);
+    let mut opts = getopts::Options::new();
+    opts.optopt("h", "html", "HTML document", "FILENAME");
+    opts.optopt("c", "css", "CSS stylesheet", "FILENAME");
+    opts.optopt("o", "output", "Output file", "FILENAME");
+    opts.optopt("f", "format", "Output file format", "png | pdf");
 
-    let css_content = read("./tests/test.css".to_string());
-    let style = css::parse(css_content);
-    assert_eq!(style.rules.len(), 8);
+    let matches = opts.parse(std::env::args().skip(1)).unwrap();
+    let str_arg = |flag: &str, default: &str| -> String {
+        matches.opt_str(flag).unwrap_or(default.to_string())
+    };
+
+    let png = match &str_arg("f", "png")[..] {
+        "png" => true,
+        "pdf" => false,
+        x => panic!("Unknown output format: {}", x),
+    };
+
+    let html = read(str_arg("h", "tests/perf-rainbow.html"));
+    let css = read(str_arg("c", "tests/perf-rainbow.css"));
+
+    let mut viewport: layout::Dimensions = Default::default();
+    viewport.content.width = 8000.0;
+    viewport.content.height = 6000.0;
+
+    let root_node = html::parse(html);
+    let stylesheet = css::parse(css);
+    let style_root = style::style_tree(&root_node, &stylesheet);
+    let layout_root = layout::layout_tree(&style_root, viewport);
+
+    let filename = str_arg("o", if png { "output.png" } else { "output.pdf" });
+    let mut file = BufWriter::new(File::create(&filename).unwrap());
+
+    let ok = if png {
+        let canvas = painting::paint(&layout_root, viewport.content);
+        let (w, h) = (canvas.width as u32, canvas.height as u32);
+        let img = image::ImageBuffer::from_fn(w, h, move |x, y| {
+            let color = canvas.pixels[(y * w + x) as usize];
+            Rgba([color.r, color.g, color.b, color.a])
+        });
+        image::DynamicImage::ImageRgba8(img).save_with_format(filename.clone(), image::ImageFormat::Png).is_ok()
+    } else {
+        pdf::render(&layout_root, viewport.content, &mut file).is_ok()
+    };
+    if ok {
+        println!("Saved output as {}", filename)
+    } else {
+        println!("Error saving output as {}", filename)
+    }
 }
 
 fn read(filename: String) -> String {
